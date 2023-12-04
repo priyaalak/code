137db555fb9950471c61363533f88a1a66acff7e
Lakshmi Priya <127914323+priyaalak@users.noreply.github.com>
2023-12-04 19:00:28 +0530

    Add files via upload

---
+++ src/components/Test.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {motion} from "framer-motion"
+
+const Test = () => {
+  return (
+    <div className='course'>
+    <motion.div className="box" 
+    initial={{ opacity:0 , scale:0.5 }}
+    animate={{opacity:0}} 
+    transion={{duration:2, delay:2}}></motion.div>
+    </div>
+  )
+}
+
+export default Test
